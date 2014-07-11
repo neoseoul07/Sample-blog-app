f0719a1a858028a58341df08da4330ca2c5f568b
Yaswanth Tavva <tavvayaswanth567@gmail.com>
2014-07-11 14:19:52 +0530

    displaying comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
 	def create
 	@blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.create(comment_params)
+    @comment.user_id=current_user.id
     redirect_to blog_path(@blog)
 end
 private
 def comment_params
-	params.require(:comment).permit(:blog_id, :body)
+	params.require(:comment).permit(:body)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711074638) do
+ActiveRecord::Schema.define(version: 20140711082555) do
 
   create_table "blogs", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140711074638) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "blog_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
