d6118347658ae335a3977baa34fa3898dea12771
Yaswanth Tavva <tavvayaswanth567@gmail.com>
2014-07-15 15:53:28 +0530

    GET#new controller spec added

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 describe BlogsController, :type => :controller  do 
+	describe "GET#new" do
+		subject{ get :new }
+		it "renders the new template" do
+      expect(subject).to render_template(:new)
+      expect(subject).to render_template("new")
+      expect(subject).to render_template("blogs/new")
+    end
+  end
 
-	
+  	#describe "GET#:id" do
+  	#	subject { :action => :show, get :id }
+  	#	it "redirects_to /widgets/:id" do
+     # expect(subject).to render_template(:show)
+    #end
+#end
+#describe "GET#blogs/:id" do
+#	it "renders the show page" do
+#expect(:get => "/blogs/1").to render_template(:show)
+#expect(:get => "/blogs/1").to render_template("show")
+#end
+#end
+
+
 end
 	
