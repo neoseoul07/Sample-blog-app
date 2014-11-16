d86265983a4c419d63d28d671316d8409f72121c
Tavva Yaswanth <yaswanth.tavva@zoomcar.in>
2014-11-16 19:59:47 +0530

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140715082841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140715082841) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
